e387fe6a852e31a794212806f2d2f854973f71bb
Alexey31 NS <webxak31@yandex.ru>
2022-07-18 19:19:08 +0300

    Добавил файл конфиг db

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'dbname' => 'db-bank',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'options' => [
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
+    ],
+];
